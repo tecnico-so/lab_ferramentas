ec74fda5d3c9a63cf63d21cfe8ce83377962a90e
bsdinis <baltasar.dinis@tecnico.ulisboa.pt>
2022-10-20 11:31:27 +0100

    [lab_ferramentas]: code review

--- src/bst.h
+++ src/bst.h
@@ -3,17 +3,17 @@
 #define DATA_SIZE 20
 
 typedef struct node {
-  long key;
-  char data[DATA_SIZE];
+    long key;
+    char data[DATA_SIZE];
 
-  struct node *left;
-  struct node *right;
-} node;
+    struct node *left;
+    struct node *right;
+} node_t;
 
-node *search(node *p, long key);
-node *insert(node *p, long key, char *data);
-node *find_min(node *p);
-node *remove_min(node *p);
-node *remove_item(node *p, long key);
-void free_tree(node *p);
-void print_tree(node *p);
+node_t *search(node_t *p, long key);
+node_t *insert(node_t *p, long key, char *data);
+node_t *find_min(node_t *p);
+node_t *remove_min(node_t *p);
+node_t *remove_item(node_t *p, long key);
+void free_tree(node_t *p);
+void print_tree(node_t *p);
--- src/test.c
+++ src/test.c
@@ -4,38 +4,42 @@
 #include <string.h>
 
 int main(void) {
-  node *root = NULL;
-  int debug = 0;
-  char c;
-  int k;
+    node_t *root = NULL;
+    int debug = 0;
+    char c = '0';
+    int k = 0;
 
-  printf("> ");
-  while (scanf("%c", &c) == 1 && c != 'q') {
-    if (c == 'a') {
-      scanf("%d", &k);
-      root = insert(root, k, "empty");
-      if (debug)
-        print_tree(root);
-    } else if (c == 's') {
-      scanf("%d", &k);
-      node *n = search(root, k);
-      if (n)
-        printf("%ld data=\"%s\"\n", n->key, n->data);
-    } else if (c == 'r') {
-      scanf("%d", &k);
-      root = remove_item(root, k);
-      if (debug)
-        print_tree(root);
-    } else if (c == 'd')
-      debug = 1 - debug;
-    else if (c == 'p')
-      print_tree(root);
+    printf("> ");
+    while (scanf("%c", &c) == 1 && c != 'q') {
+        if (c == 'a') {
+            scanf("%d", &k);
+            root = insert(root, k, "empty");
+            if (debug) {
+                print_tree(root);
+            }
+        } else if (c == 's') {
+            scanf("%d", &k);
+            node_t *n = search(root, k);
+            if (n)
+                printf("%ld data=\"%s\"\n", n->key, n->data);
+        } else if (c == 'r') {
+            scanf("%d", &k);
+            root = remove_item(root, k);
+            if (debug) {
+                print_tree(root);
+            }
+        } else if (c == 'd') {
+            debug = 1 - debug;
+        } else if (c == 'p') {
+            print_tree(root);
+        }
 
-    if (c == '\n')
-      printf("> ");
-  }
+        if (c == '\n') {
+            printf("> ");
+        }
+    }
 
-  free_tree(root);
+    free_tree(root);
 
-  return 0;
+    return 0;
 }
